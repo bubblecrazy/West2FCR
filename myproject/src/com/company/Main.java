61adf086b437091b01b2f955be751111f1e20b9b
bubblecrazy <70338419+bubblecrazy@users.noreply.github.com>
2020-12-12 22:07:10 +0800

    Add files via upload

---
+++ myproject/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+	// write your code here
+        West2FriedChickenRestaurant West2FCR = new West2FriedChickenRestaurant(5000.00,
+                new ArrayList<Beer>(),new ArrayList<Juice>());
+    }
+}
